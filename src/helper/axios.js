2c818fec2de0f5193abf4940a87d8f1c501668ec
魏建军 <weijianjun@bjflkj.com>
2025-05-29 19:29:37 +0800

    update

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,222 +1,143 @@
-;(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined'
-    ? (module.exports = factory(require('@system.fetch')))
-    : typeof define === 'function' && define.amd
-    ? define(['@system.fetch'], factory)
-    : ((global =
-        typeof globalThis !== 'undefined' ? globalThis : global || self),
-      (global.pluginQaRequest = factory(global.fetch)))
-})(this, function (fetch) {
-  'use strict'
+import $fetch from '@system.fetch'
 
-  function _interopDefaultLegacy(e) {
-    return e && typeof e === 'object' && 'default' in e ? e : { default: e }
+function absoluteUrl(url) {
+  if (
+    url &&
+    (url.startsWith('http://') ||
+      url.startsWith('https://') ||
+      url.startsWith('//'))
+  ) {
+    return true
   }
 
-  var fetch__default = /*#__PURE__*/ _interopDefaultLegacy(fetch)
+  return false
+}
 
-  function ownKeys(object, enumerableOnly) {
-    var keys = Object.keys(object)
+function isUndef(val) {
+  return val == null
+}
 
-    if (Object.getOwnPropertySymbols) {
-      var symbols = Object.getOwnPropertySymbols(object)
-      enumerableOnly &&
-        (symbols = symbols.filter(function (sym) {
-          return Object.getOwnPropertyDescriptor(object, sym).enumerable
-        })),
-        keys.push.apply(keys, symbols)
-    }
+function isObject(val) {
+  return Object.prototype.toString.call(val) === '[object Object]'
+}
 
-    return keys
+function callHook(hook, data) {
+  if ('function' === typeof hook) {
+    hook(data)
   }
+}
 
-  function _objectSpread2(target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = null != arguments[i] ? arguments[i] : {}
-      i % 2
-        ? ownKeys(Object(source), !0).forEach(function (key) {
-            _defineProperty(target, key, source[key])
-          })
-        : Object.getOwnPropertyDescriptors
-        ? Object.defineProperties(
-            target,
-            Object.getOwnPropertyDescriptors(source)
-          )
-        : ownKeys(Object(source)).forEach(function (key) {
-            Object.defineProperty(
-              target,
-              key,
-              Object.getOwnPropertyDescriptor(source, key)
-            )
-          })
+function merge(requestParams, defaults) {
+  Object.keys(defaults).forEach(function (key) {
+    if (isUndef(requestParams[key])) {
+      requestParams[key] = defaults[key]
+    } else if (isObject(requestParams.headers) && isObject(defaults.headers)) {
+      requestParams.headers = Object.assign(
+        {},
+        defaults.headers,
+        requestParams.headers
+      )
     }
+  })
 
-    return target
+  if (!absoluteUrl(requestParams.url)) {
+    requestParams.url = requestParams.baseURL + requestParams.url
   }
+}
 
-  function _defineProperty(obj, key, value) {
-    if (key in obj) {
-      Object.defineProperty(obj, key, {
-        value: value,
-        enumerable: true,
-        configurable: true,
-        writable: true,
-      })
-    } else {
-      obj[key] = value
-    }
-
-    return obj
-  }
-
-  function absoluteUrl(url) {
-    if (
-      url &&
-      (url.startsWith('http://') ||
-        url.startsWith('https://') ||
-        url.startsWith('//'))
-    ) {
-      return true
-    }
-
+function needRetry(requestParams, response) {
+  if (
+    (typeof requestParams.retryCount === 'number' &&
+      requestParams.retryCount <= 0) ||
+    !requestParams.retryFilter
+  ) {
     return false
   }
 
-  function isUndef(val) {
-    return val == null
-  }
+  return requestParams.retryFilter(response)
+}
 
-  function isObject(val) {
-    return Object.prototype.toString.call(val) === '[object Object]'
-  }
+request.defaults = {
+  baseURL: '',
+  timeout: 10000,
+  responseType: 'json',
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+  retryCount: 0,
+  retryFilter: function retryFilter(_ref) {
+    var code = _ref.code
+    return code !== 404
+  },
+}
 
-  function callHook(hook, data) {
-    if ('function' === typeof hook) {
-      hook(data)
-    }
-  }
-  function merge(requestParams, defaults) {
-    Object.keys(defaults).forEach(function (key) {
-      if (isUndef(requestParams[key])) {
-        requestParams[key] = defaults[key]
-      } else if (
-        isObject(requestParams.headers) &&
-        isObject(defaults.headers)
-      ) {
-        requestParams.headers = _objectSpread2(
-          _objectSpread2({}, defaults.headers),
-          requestParams.headers
-        )
-      }
-    })
+function request(requestParams) {
+  merge(requestParams, request.defaults)
+  callHook(requestParams['beforeRequest'], requestParams)
+  return Promise.race([
+    requestHandler(requestParams),
+    timeoutHandler(requestParams.timeout),
+  ])
+}
 
-    if (!absoluteUrl(requestParams.url)) {
-      requestParams.url = requestParams.baseURL + requestParams.url
-    }
-  }
-  function needRetry(requestParams, response) {
-    if (
-      (typeof requestParams.retryCount === 'number' &&
-        requestParams.retryCount <= 0) ||
-      !requestParams.retryFilter
-    ) {
-      return false
-    }
+function requestHandler(requestParams) {
+  return new Promise(function (resolve, reject) {
+    callHook(requestParams['startRequest'], requestParams)
+    $fetch.fetch({
+      url: requestParams.url,
+      data: requestParams.data,
+      method: requestParams.method,
+      header: requestParams.headers,
+      responseType: requestParams.responseType,
+      success: function success(response) {
+        callHook(requestParams['afterRequest'], response)
 
-    return requestParams.retryFilter(response)
-  }
-
-  request.defaults = {
-    baseURL: '',
-    timeout: 10000,
-    responseType: 'json',
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-    retryCount: 0,
-    retryFilter: function retryFilter(_ref) {
-      var code = _ref.code
-      return code !== 404
-    },
-  }
-
-  function request(requestParams) {
-    merge(requestParams, request.defaults)
-    callHook(requestParams['beforeRequest'], requestParams)
-    return Promise.race([
-      requestHandler(requestParams),
-      timeoutHandler(requestParams.timeout),
-    ])
-  }
-
-  function requestHandler(requestParams) {
-    return new Promise(function (resolve, reject) {
-      callHook(requestParams['startRequest'], requestParams)
-      fetch__default['default'].fetch({
-        url: requestParams.url,
-        data: requestParams.data,
-        method: requestParams.method,
-        header: requestParams.headers,
-        responseType: requestParams.responseType,
-        success: function success(response) {
-          callHook(requestParams['afterRequest'], response)
-
-          if (response.code == 200) {
-            if (response.data && response.data.status == 1) {
-              callHook(requestParams['responseSuccess'], response)
-              resolve(response.data.data)
-            } else {
-              if (needRetry(requestParams, response)) {
-                requestParams.retryCount--
-                requestHandler(requestParams)
-              } else {
-                callHook(requestParams['responseError'], response)
-                reject(response)
-              }
-            }
+        if (response.code == 200) {
+          if (response.data) {
+            callHook(requestParams['responseSuccess'], response)
+            resolve(response.data)
           } else {
             if (needRetry(requestParams, response)) {
               requestParams.retryCount--
               requestHandler(requestParams)
             } else {
-              callHook(requestParams['requestFail'], response)
+              callHook(requestParams['responseError'], response)
               reject(response)
             }
           }
-        },
-        fail: function fail(data, code) {
-          callHook(requestParams['requestFail'], {
-            data: data,
-            code: code,
-          })
-          reject({
-            data: data,
-            code: code,
-          })
-        },
-        complete: function complete() {
-          callHook(requestParams['finished'], null)
-        },
-      })
-    })
-  }
-
-  function timeoutHandler(timeout) {
-    return new Promise(function (_, reject) {
-      setTimeout(function () {
-        reject('网络超时！')
-      }, timeout)
-    })
-  }
-
-  function index() {
-    return {
-      name: 'request',
-      resolve: function resolve() {
-        return request
+        } else {
+          if (needRetry(requestParams, response)) {
+            requestParams.retryCount--
+            requestHandler(requestParams)
+          } else {
+            callHook(requestParams['requestFail'], response)
+            reject(response)
+          }
+        }
       },
-    }
-  }
+      fail: function fail(data, code) {
+        callHook(requestParams['requestFail'], {
+          data: data,
+          code: code,
+        })
+        reject({
+          data: data,
+          code: code,
+        })
+      },
+      complete: function complete() {
+        callHook(requestParams['finished'], null)
+      },
+    })
+  })
+}
 
-  return index
-})
+function timeoutHandler(timeout) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject('网络超时！')
+    }, timeout)
+  })
+}
+
+export default request
